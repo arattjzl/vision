89bae7152805fb33ac059b1a7e8a4b8f6f826705
Ulises Montoya <ulisesjafetmontoya@gmail.com>
2025-02-25 13:24:40 -0600

    Debugging main.py and found that dlib and the model cause the video to run slowly

--- main.py
+++ main.py
@@ -6,28 +6,26 @@ import tensorflow as tf
 import time
 from morse3 import Morse
 
+
 def getEyes(frame, face):
     landmarks = predictor(frame, face)
 
-    xtext = landmarks.part(28).x
-    ytext = landmarks.part(28).y
+    # xtext = landmarks.part(28).x
+    # ytext = landmarks.part(28).y
 
     x_left = landmarks.part(42).x
     y_left = landmarks.part(42).y
 
-    x_top = round((landmarks.part(43).x + landmarks.part(44).x) / 2)
+    # x_top = round((landmarks.part(43).x + landmarks.part(44).x) / 2)
     y_top = round((landmarks.part(43).y + landmarks.part(44).y) /2)
 
     x_right = landmarks.part(45).x
-    y_right = landmarks.part(45).y
+    # y_right = landmarks.part(45).y
 
-    x_bot = round((landmarks.part(46).x + landmarks.part(47).x) /2)
+    # x_bot = round((landmarks.part(46).x + landmarks.part(47).x) /2)
     y_bot = round((landmarks.part(46).y + landmarks.part(47).y) /2)
 
-    """ cv.circle(frame, (x_left,y_left), 3, (0,0,255), 2)
-    cv.circle(frame, (x_top,y_top), 3, (0,0,255), 2)
-    cv.circle(frame, (x_right,y_right), 3, (0,0,255), 2)
-    cv.circle(frame, (x_bot,y_bot), 3, (0,0,255), 2) """
+    
 
     xc = round((x_left + x_right) / 2)
     yc = round((y_top + y_bot) / 2)
@@ -37,28 +35,24 @@ def getEyes(frame, face):
 
     r = round((np.linalg.norm(p1 - p2))*2.5)
 
-    img = cv.circle(gray, (xc, yc), r, (0,0,255), 3)
-
-    """ cv.rectangle(frame, (round(xc - r), round(yc + r)), (round(xc+r), round(yc - r)), 3) """
-
     topx = round(yc - r)  # Coordenada superior (Y)
     topy = round(xc - r)  # Coordenada superior (X)
     botx = round(yc + r)  # Coordenada inferior (Y)
     boty = round(xc + r)  # Coordenada inferior (X)
 
-    return topx, topy, botx, boty, xtext, ytext
+    return topx, topy, botx, boty
 
     
+inputs = []
 try:
-    cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture('testFiles/hola.mp4')
     # cap.set(cv.CAP_PROP_FPS, 30)
     fps = cap.get(cv.CAP_PROP_FPS)
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')   
-    model = tf.keras.models.load_model('models/open-closed-eyes-v3.h5')
+    model = tf.keras.models.load_model('models/open-closed-eyes-v3.h5', compile=False)
     labels = ['closed', 'open']
     frame_count = 0
-    inputs = []
     last = None
     startTimerClose = startTimerOpen = ''
     totalTimeClosed = totalTimeOpen = 0
@@ -68,80 +62,79 @@ try:
 
     while cap.isOpened():
         ret, frame = cap.read()
+        width = int(frame.shape[1] * 0.25)
+        height = int(frame.shape[0] * 0.25)
+        frame = cv.resize(frame,(width, height))
+        
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = detector(frame)
+        # # if frame_count % frame_ratio == 0:
+        
+        topx, topy, botx, boty = getEyes(frame, faces[0])
 
-        if not ret:
-            print("FAIL TO READ FRAME")
+        # # Extraer la región de interés (ROI)
+        img = frame[topx:botx, topy:boty]
+        img_gray_resized = cv.resize(img, (224, 224))  # Ajustar tamaño
+        
+        cv.imshow("img", img_gray_resized)
+        
+        img_array = np.expand_dims(img_gray_resized, axis=0)  # Añadir una dimensión extra para el batch
+        img_array = img_array / 255.0  # Normalizar
 
-        # print(fps)
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        
+        # cv.imwrite("testFiles/eye.jpg", img)
         
         if frame_count % frame_ratio == 0:
-            faces = detector(gray)  
+            pred = model.predict(img_array)
+            class_id = np.argmax(pred, axis=1)[0]
+            # print("prediccion", class_id)
 
-        for face in faces:
-            topx, topy, botx, boty, xtext, ytext = getEyes(gray, face)
+        #     # CLOSED EYE 
+        #     if class_id == 0:
 
-            # Extraer la región de interés (ROI)
-            img = gray[topx:botx, topy:boty]
+        #         if not isClose:
+        #             startTimerClose = time.perf_counter()
+        #             isClose = True
 
-            img_gray_resized = cv.resize(img, (224, 224))  # Ajustar tamaño
-            img_array = np.expand_dims(img_gray_resized, axis=0)  # Añadir una dimensión extra para el batch
-            img_array = img_array / 255.0  # Normalizar
+        #         if isOpen:
+        #             endTimerOpen = time.perf_counter()
+        #             totalTimeOpen = endTimerOpen - startTimerOpen
+        #             print("OPEN", totalTimeOpen)
 
-            if frame_count % frame_ratio == 0:
-                pred = model.predict(img_array)
-                class_id = np.argmax(pred, axis=1)
-                # print("prediccion", class_id)
+        #             if totalTimeOpen > 0.7:
+        #                 inputs.append(' ')
+        #             # if totalTimeOpen > 1.5:
+        #             #     inputs.append('  ')
+                    
+        #             isOpen = False
+        #             totalTimeOpen = 0
 
-                # CLOSED EYE 
-                if class_id[0] == 0:
+        #     # OPEN EYE
+        #     if class_id == 1:
 
-                    if not isClose:
-                        startTimerClose = time.perf_counter()
-                        isClose = True
+        #         if isClose:
+        #             endTimerClose = time.perf_counter()
+        #             totalTimeClosed = endTimerClose - startTimerClose
+        #             print("CLOSE", totalTimeClosed)
 
-                    if isOpen:
-                        endTimerOpen = time.perf_counter()
-                        totalTimeOpen = endTimerOpen - startTimerOpen
-                        print("OPEN", totalTimeOpen)
+        #             if 0 < totalTimeClosed < 0.4:
+        #                 inputs.append('.')
+        #             elif 0.4 < totalTimeClosed:
+        #                 inputs.append('-')
+                    
+        #             isClose = False
+        #             totalTimeClosed = 0
 
-                        if totalTimeOpen > 0.7:
-                            inputs.append(' ')
-                        # if totalTimeOpen > 1.5:
-                        #     inputs.append('  ')
-                        
-                        isOpen = False
-                        totalTimeOpen = 0
-
-                # OPEN EYE
-                if class_id[0] == 1:
-
-                    if isClose:
-                        endTimerClose = time.perf_counter()
-                        totalTimeClosed = endTimerClose - startTimerClose
-                        print("CLOSE", totalTimeClosed)
-
-                        if 0 < totalTimeClosed < 0.4:
-                            inputs.append('.')
-                        elif 0.4 < totalTimeClosed:
-                            inputs.append('-')
-                        
-                        isClose = False
-                        totalTimeClosed = 0
-
-                    if not isOpen:
-                        startTimerOpen = time.perf_counter()
-                        isOpen = True
-                
-            # cv.putText(frame, labels[class_id[0]], (xtext, ytext), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3, cv.LINE_AA)
-            cv.imshow("img", img) 
+        #         if not isOpen:
+        #             startTimerOpen = time.perf_counter()
+        #             isOpen = True
 
         # print(inputs)
-        frame_count += 1
+        # frame_count += 1
 
         # cv.imshow('frame', frame)
 
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        if cv.waitKey(30) & 0xFF == ord('q'):
             break
 except Exception as e:
     print(e)
---
+++ testFiles/image_predictor.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('testFiles/eye.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.resize(img, (224, 224))
+img = img / 255.0
+img_array = np.array([img])
+
+model = tf.keras.models.load_model('models/open-closed-eyes-v3.h5', compile=False)
+
+predict = model.predict(img_array)
+print(predict)
+plt.imshow(img, cmap='gray')
+plt.show()
+
+
